6dddeafaa68929672005bcf9d795dca442328e9e
mashasargsyan <maryamsargsyan04@gmail.com>
2025-11-17 21:55:52 +0400

    Added Exit button

--- uno-front/my-app/src/Game.js
+++ uno-front/my-app/src/Game.js
@@ -1,21 +1,52 @@
+
 import { useLocation } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+import { useState } from "react";
 
 export default function Game() {
+  const [showExit, setShowExit] = useState(false);
   const { state } = useLocation();
+  const navigate = useNavigate();
 
   const players = state?.players || 2;
   const difficulty = state?.difficulty || "Easy";
   const name = state?.name || "John Doe";
 
+  const handleShowExit = () => {
+    setShowExit(true);
+  };
+
   return (
+    <div className = "UnoForm">
     <div className="GamePage">
-      <h1 className = "title">UNO</h1>
+      <div className = "title">UNO</div>
 
       <div className="GameDetails">
         <p><strong>Name:</strong> {name}</p>
         <p><strong>Player count:</strong> {players}</p>
         <p><strong>Complexity:</strong> {difficulty}</p>
+        <div>
+          <button onClick={handleShowExit}>Exit</button>
+        </div>
       </div>
+ 
+     {showExit && (
+        <div className="modal" onClick={() => setShowExit(false)}>   
+          <div className="modal-content" onClick={(e) => e.stopPropagation()}>
+            <h3>Are you sure?</h3>
+            
+            <div className="modal-buttons">
+              <button onClick={() => navigate("/")}>
+                YES
+              </button>
+              <button onClick={() => setShowExit(false)}>
+                NO
+              </button>
+            </div>
+          </div>
+        </div>
+      )} 
     </div>
+  </div>
   );
 }
